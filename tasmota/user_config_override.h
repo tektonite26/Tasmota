e4b34a1ede26e675a17b9a49ddf59372bd0ab650
tektonite26 <mbbk110cc@yahoo.co.id>
2026-08-02 10:40:03 +0700

    Clean up user_config_override.h by disabling features
    Removed unused feature definitions for MQTT, Domoticz, KNX, BLE, and iBeacon.

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -3,8 +3,39 @@
 
 #warning **** user_config_override.h: Using Settings from This File ****
 
-#define USE_TELNET
+// -------- Disable unwanted features --------
 
+#ifdef USE_MQTT
+#undef USE_MQTT
+#endif
+
+#ifdef USE_DOMOTICZ
+#undef USE_DOMOTICZ
+#endif
+
+#ifdef USE_KNX
+#undef USE_KNX
+#endif
+
+#ifdef USE_KNX_WEB_MENU
+#undef USE_KNX_WEB_MENU
+#endif
+
+#ifdef USE_BLE_ESP32
+#undef USE_BLE_ESP32
+#endif
+
+#ifdef USE_IBEACON_ESP32
+#undef USE_IBEACON_ESP32
+#endif
+
+#ifdef USE_MI_ESP32
+#undef USE_MI_ESP32
+#endif
+
+// -------- Keep needed features --------
+
+#define USE_TELNET
 #define USE_I2C
 #define USE_RTC_CHIPS
 #define USE_DS3231
